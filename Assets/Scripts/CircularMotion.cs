43cdad54013e335f413eb034a54cacb832f09389
ZakaryKeck <zakary.keck@huskers.unl.edu>
2020-01-22 15:30:03 -0600

    Circular Motion

---
+++ Assets/Scripts/CircularMotion.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CircularMotion : MonoBehaviour {
+    float angle = 0;
+    float speed = (2 * Mathf.PI) / 5; //2*PI in degress is 360, so you get 5 seconds to complete a circle
+    float radius = 5;
+    float x, y;
+
+    void Update()
+    {
+        angle += speed * Time.deltaTime; 
+        x = Mathf.Cos(angle)*radius;
+        y = Mathf.Sin(angle)*radius;
+
+        transform.position = new Vector3(x, 0.75f, y);
+
+    }
+}
